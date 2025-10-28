2f1fdb65cd8c7dd803de28ea942880084f81e868
Leonid Gordenin <db0zmail@gmail.com>
2025-10-28 00:24:08 -0300

    jumping and neat jumping animation :3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,19 +8,39 @@ public class Player : MonoBehaviour
 {
     private static readonly int RunTrigger = Animator.StringToHash("run_trigger");
     private static readonly int IdleTrigger = Animator.StringToHash("idle_trigger");
+    private static readonly int JumpTrigger = Animator.StringToHash("jump_trigger");
+    private static readonly int FallTrigger = Animator.StringToHash("fall_trigger");
+    private static readonly int OnGround = Animator.StringToHash("on_ground");
+    private static readonly int Walking = Animator.StringToHash("walking");
+    private static readonly int Jumping = Animator.StringToHash("jumping");
 
-    [SerializeField] public float RunSpeed;
+    [SerializeField] private float RunSpeed;
 
-    [SerializeField] public float RunAccel;
+    [SerializeField] private float RunAccel;
+
+    [SerializeField] private float JumpSpeed;
+
+    [SerializeField] private float FlipSpeed;
 
     [SerializeField] private Rigidbody2D _rigidbody;
 
     [SerializeField] private Animator _animator;
 
     [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private Transform _groundCheckPosition;
+
+    [SerializeField] private LayerMask _groundCheckLayerMask;
+    
+    [SerializeField] private Transform _selfTransform;
+
+    [SerializeField] private float _groundCheckRadius;
     
     private InputAction moveAction;
     private InputAction jumpAction;
+
+    private bool _isOnGround;
+    private bool _isJumping;
     
     void Start()
     {
@@ -30,7 +50,6 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        UpdateVisuals();
     }
 
     void FixedUpdate()
@@ -42,27 +61,42 @@ public class Player : MonoBehaviour
 
         vel = Mathf.MoveTowards(vel, targetVel, RunAccel * Time.deltaTime);
         _rigidbody.velocity = new Vector2(vel, _rigidbody.velocity.y);
+
+        _isOnGround = IsOnGround();
+        if (_isOnGround && jumpAction.WasPressedThisFrame())
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, JumpSpeed);
+            _isJumping = true;
+        }
+
+        if (_isJumping && _rigidbody.velocity.y < FlipSpeed)
+        {
+            _isJumping = false;
+        }
+        
+        UpdateVisuals();
     }
 
-    void UpdateVisuals()
+    private void UpdateVisuals()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
         
-        if (moveValue.x != 0)
-        {
-            _animator.SetTrigger(RunTrigger);
-        }
-        else
-        {
-            _animator.SetTrigger(IdleTrigger);
-        }
+        _animator.SetBool(OnGround, _isOnGround);
+        _animator.SetBool(Walking, moveValue.x != 0);
+        _animator.SetBool(Jumping, _isJumping);
 
         if (moveValue.x > 0)
         {
-            _spriteRenderer.flipX = true;
+            _selfTransform.localScale = new Vector3(-1, 1, 1);
         } else if (moveValue.x < 0)
         {
-            _spriteRenderer.flipX = false;
+            _selfTransform.localScale = Vector3.one;
         }
+    }
+
+    private bool IsOnGround()
+    {
+        var coll = Physics2D.OverlapCircle(_groundCheckPosition.position, _groundCheckRadius, _groundCheckLayerMask);
+        return coll is not null;
     }
 }
