49fe10945861cf0eeeb9b2908dde45c6d5df3d47
Leonid Gordenin <db0zmail@gmail.com>
2025-10-28 00:24:16 -0300

    camera following player

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private float _deadzoneWidthFraction;
+    
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] private Transform _cameraTransform;
+    
+    [SerializeField] private Transform _targetTransform;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        var screenHalfWidth = _camera.orthographicSize * Screen.width / Screen.height;
+        var targetX = _targetTransform.position.x;
+        var min = targetX - screenHalfWidth * _deadzoneWidthFraction;
+        var max = targetX + screenHalfWidth * _deadzoneWidthFraction;
+        
+        var pos = _cameraTransform.position;
+        pos.x = Mathf.Clamp(pos.x, min, max);
+        _cameraTransform.position = pos;
+    }
+}
